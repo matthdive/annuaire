04e2b5132b000d646465e01fd897b0615787b43f
xt <xt@pop-os.localdomain>
2022-05-31 09:43:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'annuaire',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
